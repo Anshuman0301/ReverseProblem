7611a31655f38de38b6742124aa11d1e0241baa3
Anshuman bhattacharya <77188112+Anshuman0301@users.noreply.github.com>
2021-06-09 00:13:44 +0530

    Add files via upload

---
+++ ReverseProblem.java
@@ -0,0 +1,24 @@
+package JavaCodes;
+
+import java.util.Scanner;
+
+public class ReverseProblem
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string----");
+        String s= sc.nextLine();
+        char a[]= s.toCharArray();
+        char arr[];
+        int n = s.length();
+        for(int i=0; i<n ; i++)
+        {
+            if(a[i] != ' ')
+            {
+
+            }
+
+        }
+    }
+}
